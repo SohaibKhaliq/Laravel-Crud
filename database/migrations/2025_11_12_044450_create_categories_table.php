9c9dd983c8bfe72e65c336e994165a3ad5938911
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-12 10:16:51 +0500

    Create categories migration with initial schema

--- database/migrations/2025_11_12_044450_create_categories_table.php
+++ database/migrations/2025_11_12_044450_create_categories_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
+            $table->string("name");
+            $table->string("slug")->unique();
+            $table->text("description")->nullable();
+            $table->boolean("status")->default(true);
+            $table->string("image")->nullable();
             $table->timestamps();
         });
     }
