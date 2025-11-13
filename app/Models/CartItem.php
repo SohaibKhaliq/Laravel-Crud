61fbfeb72e13598ca769947d03ecb7465a9addec
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-13 09:53:18 +0500

    Add CartItem model with fillable properties for cart items

---
+++ app/Models/CartItem.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $fillable = [
+    'cart_id',
+    'product_id',
+    'variant_id',
+    'quantity',
+    'price',
+    'subtotal',
+];
+
+}
