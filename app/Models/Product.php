7443e74a7f90eb647eb057a66dba036d2f0d690c
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-13 09:53:51 +0500

    Add Product model with fillable properties for product management

---
+++ app/Models/Product.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    protected $fillable = [
+    'category_id',
+    'sub_category_id',
+    'name',
+    'slug',
+    'sku',
+    'price',
+    'discount_price',
+    'stock',
+    'short_description',
+    'long_description',
+    'status', // 'active' / 'inactive'
+    'featured', // boolean
+];
+
+}
