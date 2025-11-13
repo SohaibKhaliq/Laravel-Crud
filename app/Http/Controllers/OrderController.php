89423fa2fb59f9d1d4890705c166f85c77a3fafa
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-13 10:07:16 +0500

    Add OrderController with CRUD operations for order management

---
+++ app/Http/Controllers/OrderController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Order;
+use Illuminate\Http\Request;
+
+class OrderController extends Controller
+{
+    public function index()
+    {
+        return response()->json(Order::all());
+    }
+
+    public function create() {}
+
+    public function store(Request $request)
+    {
+        $order = Order::create($request->all());
+        return response()->json($order, 201);
+    }
+
+    public function show(Order $order)
+    {
+        return response()->json($order);
+    }
+
+    public function edit(Order $order) {}
+
+    public function update(Request $request, Order $order)
+    {
+        $order->update($request->all());
+        return response()->json($order);
+    }
+
+    public function destroy(Order $order)
+    {
+        $order->delete();
+        return response()->noContent();
+    }
+}
