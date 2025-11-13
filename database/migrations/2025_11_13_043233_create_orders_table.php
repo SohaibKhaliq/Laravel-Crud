378cc3d5c169c306f54ff6a1b86d8d6bdd48ee03
SohaibKhaliq <sohaibkhaliq510@gmail.com>
2025-11-13 09:56:22 +0500

    Add migration for orders table with necessary fields

---
+++ database/migrations/2025_11_13_043233_create_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('order_number')->unique();
+            $table->decimal('total_amount', 12, 2)->default(0);
+            $table->decimal('discount', 12, 2)->default(0);
+            $table->decimal('shipping_cost', 12, 2)->default(0);
+            $table->string('payment_status')->default('pending');
+            $table->string('order_status')->default('pending');
+            $table->unsignedBigInteger('shipping_address_id')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
